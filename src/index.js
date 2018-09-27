51744f30d2ae66c8f9dc7cc4a6aacc1086c35d68
Angelica <angelica.arroyavem@udea.edu.co>
2018-09-27 08:37:53 -0500

    Adding the search bar with its controlled component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = 'AIzaSyC_XgR2Rb_ji392bYiI5qwp97yuvA07Hgg';
+YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+    console.log(data)
+});
 // Create a new component, 
 //this component should produce some HTML
-const App = () => <div>HI!</div>
+const App = () => {
+
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
+};
+
 
 
 //Take this component's generated HTML and put it
 // on the page (in the DOM)
 
-ReactDOM.render(<App/>, document.querySelector('.container') );
+ReactDOM.render(<App />, document.querySelector('.container'));
